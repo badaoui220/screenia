884c6e048f77ddf2db4dd7f875eb948260c320a1
Said BADAOUI <badaoui220@gmail.com>
2023-02-14 15:28:28 +0100

    fix: waiting until load

--- pages/api/screenshot.ts
+++ pages/api/screenshot.ts
@@ -45,6 +45,7 @@ export default async function handler(
     await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 1 });
     await page.goto(url as string, {
       timeout: 15 * 1000,
+      waitUntil: ["load", "domcontentloaded", "networkidle0"],
     });
     await page.waitForTimeout(2000);
     const screenshot = await page.screenshot(outPut);
