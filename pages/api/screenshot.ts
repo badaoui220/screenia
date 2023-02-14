2a230bcafc0d6edc6b7b34d44f1660c3d5dbbf56
Said BADAOUI <badaoui220@gmail.com>
2023-02-14 15:36:00 +0100

    fix: remove timer

--- pages/api/screenshot.ts
+++ pages/api/screenshot.ts
@@ -43,9 +43,7 @@ export default async function handler(
     browser = await puppeteer.launch(options);
     const page = await browser.newPage();
     await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 1 });
-    await page.goto(url as string, {
-      waitUntil: ["load", "networkidle0"],
-    });
+    await page.goto(url as string);
     await page.waitForTimeout(2000);
     const screenshot = await page.screenshot(outPut);
     if (download) {
