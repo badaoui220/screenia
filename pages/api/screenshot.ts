42f22d4ef58408e1a50cde60d537216835fd1899
Said BADAOUI <badaoui220@gmail.com>
2023-02-17 16:10:09 +0100

    fix: fix api issues related to perf

--- pages/api/screenshot.ts
+++ pages/api/screenshot.ts
@@ -1,63 +1,33 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-const puppeteer = require("puppeteer-core");
-const chrome = require("chrome-aws-lambda");
+import chromium from "chrome-aws-lambda";
 
-const exePath = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+async function getBrowserInstance() {
+  const executablePath = await chromium.executablePath;
 
-async function getOptions() {
-  const env = process.env.NODE_ENV;
-  let options;
-  if (env == "development") {
-    options = {
-      args: [],
-      executablePath: exePath,
+  if (!executablePath) {
+    // running locally
+    const puppeteer = require("puppeteer");
+    return puppeteer.launch({
+      args: chromium.args,
       headless: true,
-    };
-  } else {
-    options = {
-      args: [
-        ...chrome.args,
-        "--autoplay-policy=user-gesture-required",
-        "--disable-background-networking",
-        "--disable-background-timer-throttling",
-        "--disable-backgrounding-occluded-windows",
-        "--disable-breakpad",
-        "--disable-client-side-phishing-detection",
-        "--disable-component-update",
-        "--disable-default-apps",
-        "--disable-dev-shm-usage",
-        "--disable-domain-reliability",
-        "--disable-extensions",
-        "--disable-features=AudioServiceOutOfProcess",
-        "--disable-hang-monitor",
-        "--disable-ipc-flooding-protection",
-        "--disable-notifications",
-        "--disable-offer-store-unmasked-wallet-cards",
-        "--disable-popup-blocking",
-        "--disable-print-preview",
-        "--disable-prompt-on-repost",
-        "--disable-renderer-backgrounding",
-        "--disable-setuid-sandbox",
-        "--disable-speech-api",
-        "--disable-sync",
-        "--hide-scrollbars",
-        "--ignore-gpu-blacklist",
-        "--metrics-recording-only",
-        "--mute-audio",
-        "--no-default-browser-check",
-        "--no-first-run",
-        "--no-pings",
-        "--no-sandbox",
-        "--no-zygote",
-        "--password-store=basic",
-        "--use-gl=swiftshader",
-        "--use-mock-keychain",
-      ],
-      executablePath: await chrome.executablePath,
-      headless: chrome.headless,
-    };
+      defaultViewport: {
+        width: 1920,
+        height: 1080,
+      },
+      ignoreHTTPSErrors: true,
+    });
   }
-  return options;
+
+  return chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: {
+      width: 1920,
+      height: 1080,
+    },
+    executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
 }
 
 export default async function handler(
@@ -71,17 +41,13 @@ export default async function handler(
   if (!url) {
     return res.status(400).json({ error: "URL parameter is required" });
   }
-
-  const options = await getOptions();
+  let browser = null;
 
   try {
-    const browser = await puppeteer.launch(options);
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 1 });
-    await page.goto(url as string, { waitUntil: "networkidle2" });
-
+    browser = await getBrowserInstance();
+    let page = await browser.newPage();
+    await page.goto(url);
     const screenshot = await page.screenshot(outPut);
-    await browser.close();
 
     if (download) {
       res.status(200).send(screenshot);
@@ -92,11 +58,9 @@ export default async function handler(
     }
   } catch (error) {
     res.status(500).json({ error: (error as any).message });
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
   }
 }
-
-export const config = {
-  api: {
-    bodyParser: process.env.NODE_ENV !== "production",
-  },
-};
