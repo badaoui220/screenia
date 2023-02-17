20ed76170a3de53d7c12164e5d0e84f0c7ae412b
Said BADAOUI <badaoui220@gmail.com>
2023-02-17 13:07:21 +0100

    fix: fix fullpage screenshot issue

--- pages/api/screenshot.ts
+++ pages/api/screenshot.ts
@@ -28,9 +28,8 @@ export default async function handler(
   res: NextApiResponse,
 ) {
   const { url, type = "png", download = false, fullscreen = false } = req.query;
-  const height = !fullscreen ? 1080 : 0;
 
-  const outPut = { type };
+  const outPut = { type, fullPage: fullscreen };
 
   if (!url) {
     return res.status(400).json({ error: "URL parameter is required" });
@@ -41,7 +40,7 @@ export default async function handler(
   try {
     const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
-    await page.setViewport({ width: 1920, height, deviceScaleFactor: 1 });
+    await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 1 });
     await page.goto(url as string);
 
     const screenshot = await page.screenshot(outPut);
